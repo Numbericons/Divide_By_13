457f0b35bb89eb3a040d37a2b5ccab392b150572
Numbericons <zachjoliver@gmail.com>
2020-02-28 13:10:17 -0800

    Add comments

--- div13.js
+++ div13.js
@@ -1,3 +1,9 @@
+//multiply right most value in n by left most value in remainders
+//add to multiplied second right most value in n by second left most value in remainders
+//repeat wrapping around until we generate a sum
+//if sum is equal in initial arg return either
+//else return result of recursive call to function with sum
+
 function thirt(n) {
   const remainders = [1, 10, 9, 12, 3, 4];
   const num = n.toString();
